7ed241c334b94e1ca25ffd163f16b199113fa4bb
Vaishnavi0711-hub <vaishnavisai982@gmail.com>
2026-04-23 23:05:24 +0530

    UC1: Feet measurement equality check

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,2 +1,29 @@
+/**
+ * QuantityMeasurementApp
+ *
+ * This class checks equality of two values in feet.
+ * It validates input and compares values.
+ *
+ * @author Charan
+ * @version 1.0
+ */
+
 public class QuantityMeasurementApp {
+
+    // Method to check equality
+    public boolean isEqual(double value1, double value2) {
+        return value1 == value2;
+    }
+
+    public static void main(String[] args) {
+
+        QuantityMeasurementApp app = new QuantityMeasurementApp();
+
+        double feet1 = 5.0;
+        double feet2 = 5.0;
+
+        boolean result = app.isEqual(feet1, feet2);
+
+        System.out.println("Are both measurements equal? " + result);
+    }
 }
